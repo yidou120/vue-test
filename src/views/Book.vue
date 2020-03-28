89666718f376f4e8a6fd54d60146a34f9fb85f35
yidou120 <1596510418@qq.com>
2020-03-28 17:13:25 +0800

    加入axios插件,实现异步请求

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -1,10 +1,52 @@
 <template>
-    $END$
+    <div>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Author</td>
+            </tr>
+            <tr v-for="item in books">
+                <td>{{item.id}}</td>
+                <td>{{item.name}}</td>
+                <td>{{item.author}}</td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <script>
     export default {
-        name: ""
+        name: "Book",
+        data() {
+            return {
+                msg: 'test',
+                books:[
+                    {
+                        id:1,
+                        name:'张一',
+                        author:'张一啊'
+                    },
+                    {
+                        id:2,
+                        name:'张二',
+                        author:'张二啊'
+                    },
+                    {
+                        id:3,
+                        name:'张三',
+                        author:'张三啊'
+                    }
+                ]
+            }
+        },
+        created() {
+            var _this = this;
+            axios.get("http://localhost:8082/books/findAll").then(function (resp) {
+                _this.books = resp.data;
+                console.log(resp)
+            })
+        }
     }
 </script>
 
